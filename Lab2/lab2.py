2ac3637109829e2452115b14400d6bd64fdc77e6
Jonathan Hess <84877315+Jetsama@users.noreply.github.com>
2023-10-02 08:20:36 -0500

    moved files into lab 2 folder

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,7 +1,13 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from PythonModule.filemanager import fileRead
+
 
 from scipy.signal import find_peaks
 
